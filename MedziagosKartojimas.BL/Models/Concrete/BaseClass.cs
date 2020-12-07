329ca9d929ceb8e1132a0ba1b1ef93b98caa4937
rokas.lileikis <rokas.lileikis@gmail.com>
2020-12-07 20:56:13 +0200

    Uzduotis 1

---
+++ MedziagosKartojimas.BL/Models/Concrete/BaseClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedziagosKartojimas.BL.Models.Concrete
+{
+	class BaseClass
+	{
+	}
+}
---
+++ MedziagosKartojimas/BaseClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedziagosKartojimas
+{
+	public abstract class BaseClass
+	{
+		public readonly string name;
+
+		public BaseClass(string Name)
+		{
+
+			name = Name;
+		}
+
+		public abstract string NameMetodas();
+
+		public abstract int NameMetodas(int a);
+
+
+	}
+
+}

e4cd3997ebe9b539dad27ab1df457c57cf958a8b
rokas.lileikis <rokas.lileikis@gmail.com>
2020-12-08 19:42:27 +0200

    -added BKlase

---
+++ MedziagosKartojimas/BKlase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedziagosKartojimas
+{
+	public class BKlase : AKlase, IZodisKitaip
+	{
+		public BKlase(string name) : base(name)
+		{
+		}
+
+		string IZodisKitaip.ZodisBeBalsiu()
+		{
+			throw new NotImplementedException();
+		}
+
+		string IZodisKitaip.ZodisBePrieBalsiu()
+		{
+			throw new NotImplementedException();
+		}
+
+		string IZodisKitaip.ZodisSuPakeistomisBalsemis(char[] letter)
+		{
+			throw new NotImplementedException();
+		}
+
+		string IZodisKitaip.ZodisSuPakeistomisPrieBalsemis(int num)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

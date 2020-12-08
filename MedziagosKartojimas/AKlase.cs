67eab0f949aaf43483ae3c8895bb571ce23aed8e
rokas.lileikis <rokas.lileikis@gmail.com>
2020-12-08 19:10:30 +0200

    fixed

--- MedziagosKartojimas/AKlase.cs
+++ MedziagosKartojimas/AKlase.cs
@@ -11,12 +11,27 @@ namespace MedziagosKartojimas
 		}
 		public override string NameMetodas()
 		{
-			throw new NotImplementedException();
+			List<char> Balses = new List<char>{'a', 'e', 'y', 'u', 'i', 'o'};
+			StringBuilder sb = new StringBuilder();
+			var zodis = Name;
+			for (int i = 0; i < zodis.Length; i++)
+			{
+				char raide = zodis[i];
+				if (Balses.Contains(raide))	sb.Append((int)raide);
+				else sb.Append(raide);
+			}
+			return sb.ToString();
 		}
 
 		public override int NameKodas(int a)
 		{
-			throw new NotImplementedException();
+			var zodis = Name;
+			int sum = 0;
+			for (int i = 0; i < zodis.Length; i++)
+			{
+				sum += (int)zodis[i] * a;
+			}
+			return sum;
 		}
 	}
 }
